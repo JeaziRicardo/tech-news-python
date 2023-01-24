5f4245ef3c05084e6543be74b8a0120a8131b859
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-01-24 19:48:35 -0300

    feat(07): implementa a função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from ..database import search_news
+import datetime
 
 
 # Requisito 6
@@ -13,7 +14,18 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    news_list = []
+    try:
+        format_date = datetime.datetime.strptime(date, "%Y-%m-%d").strftime(
+            "%d/%m/%Y"
+        )
+        query = {"timestamp": {"$regex": format_date}}
+        search_list = search_news(query)
+        for new in search_list:
+            news_list.append((new["title"], new["url"]))
+        return news_list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
