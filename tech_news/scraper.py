0c66576ba0fc7f136f8b92a1b8fd579c8516d60c
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-01-24 16:41:53 -0300

    feat(02): implementa a função scrape_updates

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -16,7 +17,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    links_list = selector.css(".entry-thumbnail div a::attr(href)").getall()
+    return links_list
 
 
 # Requisito 3
